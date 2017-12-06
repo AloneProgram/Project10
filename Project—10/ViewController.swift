8466cd8c9648277b369c435b4429c8162168093e
AloneProgram <409222846@qq.com>
2017-12-06 15:04:56 +0800

    Swift使用UITableView系统控件下拉刷新

--- Project—10/ViewController.swift
+++ Project—10/ViewController.swift
@@ -8,13 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: VideoSplashViewController {
 
+    @IBOutlet weak var loginBtn: UIButton!
+    @IBOutlet weak var signUpBtn: UIButton!
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupVideoBackground()
+        
+        loginBtn.layer.cornerRadius = 4
+        signUpBtn.layer.cornerRadius = 4
     }
 
+    func setupVideoBackground() {
+        let url = NSURL.fileURLWithPath(NSBundle.mainBundle().pathForResource("moments", ofType: "mp4")!)
+        
+        videoFrame = view.frame
+        fillMode = .ResizeAspectFill
+        alwaysRepeat = true
+        sound = true
+        startTime = 2.0
+        alpha = 0.8
+        
+        contentURL = url
+       // view.userInteractionEnabled = false
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
